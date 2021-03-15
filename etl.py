e6078b160690b6a24dbb2a0b0e586bed8bc3c246
Jacob Shore <jshore@datalign.net>
2021-03-15 11:57:02 +0200

    format and test data

--- etl.py
+++ etl.py
@@ -1,22 +1,40 @@
 import glob
 import os
-
+from dotenv import load_dotenv
 import pandas as pd
 import psycopg2
 
 from sql_queries import *
 
+load_dotenv()
+password = os.getenv("PASSWORD")
+user = os.getenv("USER")
+
+# For debugging
+conn = psycopg2.connect(
+    f"host=127.0.0.1 dbname=sparkifydb user={user} password={password}"
+)
+curr = conn.cursor()
+
 
 def process_song_file(cur, filepath):
     # open song file
-    df = pd.read_json(filepath, typ='series')
+    df = pd.read_json(filepath, typ="series")
 
     # insert song record
-    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values
+    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values
     cur.execute(song_table_insert, song_data)
 
     # insert artist record
-    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values
+    artist_data = df[
+        [
+            "artist_id",
+            "artist_name",
+            "artist_location",
+            "artist_latitude",
+            "artist_longitude",
+        ]
+    ].values
     cur.execute(artist_table_insert, artist_data)
 
 
@@ -25,27 +43,27 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df[df['page'] == 'NextSong']
+    df = df[df["page"] == "NextSong"]
 
     # convert timestamp column to datetime
     # t =
-    df.loc[:, 'ts'] = pd.to_datetime(df['ts'], unit='ms')
-    df.loc[:, 'weekday'] = df['ts'].dt.day_name()
-    df.loc[:, 'hour'] = df['ts'].dt.hour
-    df.loc[:, 'day'] = df['ts'].dt.day
-    df.loc[:, 'week'] = df['ts'].dt.isocalendar().week
-    df.loc[:, 'month'] = df['ts'].dt.month
-    df.loc[:, 'year'] = df['ts'].dt.year
+    df.loc[:, "ts"] = pd.to_datetime(df["ts"], unit="ms")
+    df.loc[:, "weekday"] = df["ts"].dt.day_name()
+    df.loc[:, "hour"] = df["ts"].dt.hour
+    df.loc[:, "day"] = df["ts"].dt.day
+    df.loc[:, "week"] = df["ts"].dt.isocalendar().week
+    df.loc[:, "month"] = df["ts"].dt.month
+    df.loc[:, "year"] = df["ts"].dt.year
     # insert time data records
     # time_data =
-    column_labels = ['ts', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    column_labels = ["ts", "hour", "day", "week", "month", "year", "weekday"]
     time_df = df.loc[:, column_labels]
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df.loc[:, ['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = df.loc[:, ["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -62,37 +80,39 @@ def process_log_file(cur, filepath):
             songid, artistid = results
         else:
             songid, artistid = None, None
-
+        print(results)
         # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
+        # songplay_data = ""
+        # cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.json'))
+        files = glob.glob(os.path.join(root, "*.json"))
         for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student"
+    )
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
--- sql_queries.py
+++ sql_queries.py
@@ -8,16 +8,16 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = """
 CREATE TABLE IF NOT EXISTS songplays (
 song_id varchar primary key not null,
 start_time timestamp,
 user_id varchar,
 user_agent text
 );
-""")
+"""
 
-user_table_create = ("""
+user_table_create = """
 CREATE TABLE IF NOT EXISTS users (
 user_id varchar primary key not null,
 first_name varchar,
@@ -25,9 +25,9 @@ last_name varchar,
 gender varchar,
 level varchar
 );
-""")
+"""
 
-song_table_create = ("""
+song_table_create = """
 CREATE TABLE IF NOT EXISTS songs (
     song_id varchar primary key not null,
     title text,
@@ -35,9 +35,9 @@ CREATE TABLE IF NOT EXISTS songs (
     year integer,
     duration float8
 );
-""")
+"""
 
-artist_table_create = ("""
+artist_table_create = """
 CREATE TABLE IF NOT EXISTS artists (
     artist_id varchar primary key not null,
     name varchar,
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS artists (
     latitude float,
     longitude float
 );
-""")
+"""
 
-time_table_create = ("""
+time_table_create = """
 CREATE TABLE IF NOT EXISTS time (
     start_time timestamp not null,
     hour varchar,
@@ -57,39 +57,56 @@ CREATE TABLE IF NOT EXISTS time (
     year integer,
     weekday varchar
 );
-""")
+"""
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+insert into songplays (song_id, start_time, user_id, user_agent) 
+VALUES (%s)
+"""
 
-user_table_insert = ("""
-insert into users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
-""")
+user_table_insert = """
+insert into users (user_id, first_name, last_name, gender, level) 
+VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
+"""
 
-song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
-""")
+song_table_insert = """
+INSERT INTO songs (song_id, title, artist_id, year, duration) 
+VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
+"""
 
-artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
-""")
+artist_table_insert = """
+INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+VALUES (%s, %s, %s, %s, %s) on conflict do nothing 
+"""
 
-time_table_insert = ("""
-insert into time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) on conflict do nothing 
-""")
+time_table_insert = """
+insert into time (start_time, hour, day, week, month, year, weekday) 
+VALUES (%s, %s, %s, %s, %s, %s, %s) on conflict do nothing 
+"""
 
 # FIND SONGS
 
-song_select = ("""
+song_select = """
 select s.song_id, s.artist_id, a.name from songs as s 
 join artists as a on (a.artist_id = s.artist_id) 
 where s.title=%s and a.name=%s and s.duration=%s
-""")
+"""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create,
-                        time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
